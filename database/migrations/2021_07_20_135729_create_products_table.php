6eace14f22d5e624cfef83626a8901be01057aa0
unknown <thecong1996@gmail.com>
2021-07-20 21:24:38 +0700

    database

---
+++ database/migrations/2021_07_20_135729_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_product', 10);
+            $table->string('name_product', 255);
+            $table->tinyInteger('unit')->default(0);
+            $table->integer('quantity');               
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2021_07_20_135804_create_bill_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('bill_id'); 
+            $table->string('code_product', 10);
+            $table->integer('quantity_bill');                       
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_details');
+    }
+}
